3cda454a7bffaaa24487a5aaf3795ad47e69b4b2
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-08-10 00:03:46 -0700

    Modified the home route and added a template for rendering

--- app/views.py
+++ app/views.py
@@ -1,9 +1,23 @@
 """Request handlers for the microblog."""
+from flask import render_template
 from app import app
+
+
+POSTS = [
+    {
+        'author': {'nickname': 'John'},
+        'body': 'Beautiful day in Seattle'
+    },
+    {
+        'author': {'nickname': 'Susan'},
+        'body': 'The Avengers movie was so good!'
+    },
+]
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     """The home page for the Flask microblog."""
-    return "Hello, World!"
+    user = {'nickname': 'Nick'}
+    return render_template('index.html', title='Home', user=user, posts=POSTS)
