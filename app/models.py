0c98484ac8f89b0dbb0a819b3828d2db402bd8d5
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-08-10 20:57:33 -0700

    Added the Post model for individual blog posts

--- app/models.py
+++ app/models.py
@@ -9,7 +9,22 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Unicode(length=80), index=True, unique=True)
     password = db.Column(db.Unicode)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         """String representation of the User model."""
-        return '<User {self.username} | id: {self.id}>'
+        return f'<User {self.username} | id: {self.id}>'
+
+
+class Post(db.Model):
+    """The Post model."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Unicode, unique=True)
+    body = db.Column(db.Unicode)
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        """String representation of the Post model."""
+        return f'<Post {self.title}>'
