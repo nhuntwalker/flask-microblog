84d16f2ec37b2bd784a9a12171e7812550c4bed6
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-08-12 08:45:01 -0700

    added a second test for the login form on get request.

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #!flask/bin/python
-import unittest
-
 from app import app
+from bs4 import BeautifulSoup as Soup
+import unittest
 
 
 class TestCase(unittest.TestCase):
@@ -20,6 +20,17 @@ class TestCase(unittest.TestCase):
         self.assertTrue(
             'http://localhost/login' in response.location
         )
+
+    def test_login_route_shows_post_form_with_csrf_token(self) -> None:
+        """."""
+        response = self.client.get('/login')
+        html = Soup(response.data, 'html.parser')
+        self.assertIsNotNone(html.find('form', {'name': 'login_form'}))
+        form = html.find('form', {'name': 'login_form'})
+        self.assertTrue(form.attrs['method'] == 'POST')
+        self.assertIsNotNone(html.find('input', {'name': 'csrf_token'}))
+        self.assertIsNotNone(html.find('input', {'name': 'username'}))
+        self.assertIsNotNone(html.find('input', {'name': 'password'}))
 
 if __name__ == '__main__':
     unittest.main()
